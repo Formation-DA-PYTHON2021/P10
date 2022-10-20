16898b77ead079aa2644fc59ae715c7b7e1884f9
Tiffany CALVET <tiffany.calvet@developpement-durable.gouv.fr>
2022-10-20 14:19:16 +0200

    7e commit : ajout page signup + authentication JWT

--- SoftDesk/SoftDesk/settings.py
+++ SoftDesk/SoftDesk/settings.py
@@ -106,7 +106,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.1/topics/i18n/
 
-LANGUAGE_CODE = 'fr_fr'
+LANGUAGE_CODE = 'fr-fr'
 
 TIME_ZONE = 'UTC'
 
